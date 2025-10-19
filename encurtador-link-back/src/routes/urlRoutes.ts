11b63ef105a6fe19271f7108836f721abbc39d9f
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2025-10-18 23:30:20 -0300

    estrutura, inicio de drizzle, schema e generateCode

--- encurtador-link-back/src/routes/urlRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/urlRoutes.ts
-
-import { FastifyInstance } from "fastify";
-import { createShortUrl, getOriginalUrl } from "../controllers/urlController.js";
-
-export default async function urlRoutes(app: FastifyInstance) {
-  app.post("/encurtar", createShortUrl);
-  app.get("/:shortCode", getOriginalUrl);
-}
---
+++ src/db/schema.js
@@ -0,0 +1,10 @@
+import { pgTable, serial, text, varchar, integer, timestamp } from "drizzle-orm/pg-core";
+
+export const links = pgTable("links", {
+  id: serial("id").primaryKey(),
+  legenda: varchar("legenda", { length: 255 }).notNull(),
+  url_original: text("url_original").notNull(),
+  codigo: varchar("codigo", { length: 10 }).notNull().unique(),
+  cliques: integer("cliques").default(0).notNull(),
+  criado_em: timestamp("criado_em").defaultNow().notNull(),
+});
